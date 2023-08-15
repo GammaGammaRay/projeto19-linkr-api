c2c327ef8f89ee955aea147682e1d6c01f877ca8
Pedro Nascimento <sn.jaypee@gmail.com>
2023-08-15 15:59:38 -0300

    feat: add database connection

---
+++ src/database.js
@@ -0,0 +1,13 @@
+import pg from "pg"
+import dotenv from "dotenv"
+
+const { Pool } = pg;
+dotenv.config();
+
+const configDatabase = {
+    connectionString: process.env.DATABASE_URL,
+};
+
+if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
+
+export const db = new Pool(configDatabase);
