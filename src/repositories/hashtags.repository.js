e75dd91d264767f010b1330149b75bca9f2b6273
GammaGammaRay <benjamin.peret@gmail.com>
2023-08-20 15:53:04 -0300

    fix: insertNewTag

--- src/repositories/hashtags.repository.js
+++ src/repositories/hashtags.repository.js
@@ -1,27 +1,25 @@
 import { db } from "../database/databaseConnection";
 
 export async function getPostsByTagDB(tagName) {
-    const tagQueryResult = await db.query(
-      `SELECT id FROM tags WHERE name = $1`,
-      [tagName]
-    );
-  
-    if (tagQueryResult.rowCount === 0) {
-      return [];
-    }
-  
-    const tagId = tagQueryResult.rows[0].id;
-  
-    const postsQueryResult = await db.query(
-      `SELECT * FROM posts
+  const tagQueryResult = await db.query(`SELECT id FROM tags WHERE name = $1`, [
+    tagName,
+  ]);
+
+  if (tagQueryResult.rowCount === 0) {
+    return [];
+  }
+
+  const tagId = tagQueryResult.rows[0].id;
+
+  const postsQueryResult = await db.query(
+    `SELECT * FROM posts
        JOIN posts_tags ON posts.id = posts_tags."postId"
        WHERE posts_tags."tagId" = $1`,
-      [tagId]
-    );
-  
-    return postsQueryResult.rows;
-  }
-  
+    [tagId]
+  );
+
+  return postsQueryResult.rows;
+}
 
 export async function getTrendingTagsDB() {
   const result = await db.query(
@@ -36,3 +34,30 @@ export async function getTrendingTagsDB() {
   if (result.rowCount === 0) return null;
   return result.rows[0];
 }
+
+export async function insertTagDB(tagName) {
+    const tagQueryResult = await db.query(
+      `SELECT * FROM tags WHERE name = $1`,
+      [tagName]
+    );
+  
+    if (tagQueryResult.rowCount > 0) {
+      return tagQueryResult.rows[0];
+    }
+  
+    const query = `
+      INSERT INTO tags (name)
+      VALUES ($1)
+      RETURNING *;
+    `;
+  
+    const values = [tagName];
+  
+    try {
+      const result = await db.query(query, values);
+      return result.rows[0];
+    } catch (error) {
+      console.error("Error inserting new tag:", error.message);
+      throw error;
+    }
+  }
--- src/routes/hashtags.routes.js
+++ src/routes/hashtags.routes.js
@@ -2,12 +2,12 @@ import { Router } from "express";
 import { schemaValidation } from "../middlewares/schemaValidation.js";
 import { signinSchema, signupSchema } from "../schemas/authSchema.js";
 import { tokenValidation } from "../middlewares/tokenValidation.js";
-import { getPostsByTag, getTrendingTags } from "../controllers/hashtagsControllers.js";
+import { getPostsByTag, getTrendingTags, postNewTag } from "../controllers/hashtagsControllers.js";
 
 const hashtagRouter = Router();
 
+hashtagRouter.post('hashtag/:tagName', tokenValidation, postNewTag)
 hashtagRouter.get('/trending', tokenValidation, getTrendingTags);
-hashtagRouter.post('hashtag/:tagName')
-hashtagRouter.get('/hashtag/:tagName', tokenValidation, getPostsByTag);
+hashtagRouter.get('/hashtag/:tagNameg', tokenValidation, getPostsByTag);
 
 export default hashtagRouter;
