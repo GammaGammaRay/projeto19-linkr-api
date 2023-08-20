e459db3806714b86242975a1358b39755ee321d2
GammaGammaRay <benjamin.peret@gmail.com>
2023-08-20 15:52:34 -0300

    feat: insertNewTag

--- src/controllers/hashtagsControllers.js
+++ src/controllers/hashtagsControllers.js
@@ -4,7 +4,7 @@ import { getPostsByTagDB, getTrendingTagsDB } from "../repositories/hashtags.rep
 export async function getTrendingTags(req, res) {
   
     try {
-      const users = (await getTrendingTagsDB());
+      const users = await getTrendingTagsDB();
       res.status(200).send(users);
     } catch (err) {
       console.log(err);
@@ -13,6 +13,23 @@ export async function getTrendingTags(req, res) {
   }
 
   export async function getPostsByTag(req, res) {
+    const { tagName } = req.params;
+  
+    try {
+      const posts = await getPostsByTagDB(tagName);
+  
+      if (posts.length === 0) {
+        res.status(404).send("Hashtag not found");
+      } else {
+        res.status(200).send(posts);
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err.message);
+    }
+  }
+
+  export async function postNewTag(req, res) {
     const { tagName } = req.params;
   
     try {
