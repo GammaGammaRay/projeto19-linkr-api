62b5cc83764f39f7f7e6a81b2e35d9f7ad72745c
Pedro Nascimento <47681197+PPAKruNN@users.noreply.github.com>
2023-08-21 14:49:48 -0300

    Add URI to link

--- src/schemas/posts.schema.js
+++ src/schemas/posts.schema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const postSchema = Joi.object({
   author: Joi.number().required(),
-  link: Joi.string().required(),
+  link: Joi.string().uri().required(),
   description: Joi.string().allow("").optional(),
 });
 
