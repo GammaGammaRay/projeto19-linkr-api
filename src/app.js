9695ae53ccec01f653be1c77a8291524389baf71
Wesley da Silva Ribeiro <wesleysilvaribeiro9@gmail.com>
2023-08-22 17:57:05 -0300

    [FIX] fixed hashtag's posts query

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import { indexRoute } from './routes/indexRoute.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { indexRoute } from "./routes/indexRoute.js";
 
 dotenv.config();
 
@@ -11,5 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(indexRoute);
 
-const port = process.env.PORT || 5000
-app.listen(port,()=>{ console.log(`RUNNING IN PORT ${port}`)});
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`RUNNING IN PORT ${port}`);
+});
