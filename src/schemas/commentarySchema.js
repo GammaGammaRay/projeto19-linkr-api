8c801e559b9f13b85c04322691f7e73eca11cf80
Pedro Nascimento <sn.jaypee@gmail.com>
2023-08-23 16:42:16 -0300

    fix: remove author from commentarySchema since token already have it

--- src/schemas/commentarySchema.js
+++ src/schemas/commentarySchema.js
@@ -1,7 +1,6 @@
 import Joi from "joi";
 
 export const commentarySchema = Joi.object({
-  author: Joi.number().integer().positive().required(),
   postId: Joi.number().integer().positive().required(),
   description: Joi.string().required(),
 });
